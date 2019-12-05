5e90a6b1cf0915808fa60a4249d4ec7c3e66c583
oscar victoria <oscarvictoriagonzalez@gmail.com>
2019-12-04 23:51:42 -0500

    Added table view

--- Jokes/ViewController.swift
+++ Jokes/ViewController.swift
@@ -9,27 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        loadJokes()
-        
-    }
-    func loadJokes() {
-        JokesAPIClient.fetchJokes { result in
-            switch result {
-            case .failure(let error):
-                let alertController = UIAlertController(title:"Network Error", message: "\(error)", preferredStyle: .alert)
-                let okAction = UIAlertAction(title: "ok", style: .default, handler: nil)
-                alertController.addAction(okAction)
-                DispatchQueue.main.async {
-                    self.present(alertController,animated: true, completion: nil)
-                }
-            case .success(let jokes):
-                dump(jokes)
-            }
+    
+@IBOutlet weak var tableView: UITableView!
+    
+    var theJokes = [Joke]() {
+        didSet {
+            tableView.reloadData()
         }
     }
+    
 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        loadJokes()
+        
+        
+    }
+    
+  
+    
+    
+    func loadJokes() {
+        
+        theJokes = [Joke]()
+//        JokesAPIClient.fetchJokes { result in
+//            switch result {
+//            case .failure(let error):
+//                let alertController = UIAlertController(title:"Network Error", message: "\(error)", preferredStyle: .alert)
+//                let okAction = UIAlertAction(title: "ok", style: .default, handler: nil)
+//                alertController.addAction(okAction)
+//                DispatchQueue.main.async {
+//                    self.present(alertController,animated: true, completion: nil)
+//                }
+//            case .success(let jokes):
+//                dump(jokes)
+//            }
+//        }
+//    }
+
+}
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return theJokes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "jokeCell", for: indexPath)
+        let jokes = theJokes[indexPath.row]
+        cell.textLabel?.text = jokes.setup
+        return cell
+    }
 }
 
